66c8a91dd61b7d9e0d62e040f360817ef38e8054
jetty8013 <jetty8013@gmail.com>
2024-04-04 08:17:09 +0900

    슬랙메세지

---
+++ index.js
@@ -0,0 +1,23 @@
+const { createServer } = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { createMessageAdapter } = require('@slack/interactive-messages');
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const port = process.env.PORT || 3000;
+const slackInteractions = createMessageAdapter(slackSigningSecret);
+
+// Create an express application
+const app = express();
+
+// Plug the adapter in as a middleware
+app.use('/shortcuts', slackInteractions.requestListener());
+
+// Example: If you're using a body parser, always put it after the message adapter in the middleware stack
+app.use(bodyParser());
+
+// Initialize a server for the express app - you can skip this and the rest if you prefer to use app.listen()
+const server = createServer(app);
+server.listen(port, () => {
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+});
