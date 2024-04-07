bfdea6b3b68d2ced546a87bc7f7d1ce9f26d6a4f
jetty8013 <jetty8013@gmail.com>
2024-04-08 00:11:37 +0900

    add mission log test

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.shortcut('slackShortcuts', async ({ shortcut, ack }) => {
 
         const apiKey = process.env.SHEET_API_KEY;
 
-        const data = ['test1', 'test2', 'test3'];
+        const data = [['test1', 'test2', 'test3']];
 
         try {
           const response = await axios.post(`https://port-0-os-tool-server-17xco2nlss79qxq.sel5.cloudtype.app/api/add-row-mission-log?api_key=${apiKey}`, {
