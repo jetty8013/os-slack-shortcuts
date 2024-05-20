a12101e93c5ca9ee283c816481127fb79ffb3583
jetty8013 <jetty8013@gmail.com>
2024-05-20 19:51:14 +0900

    add setup modal4

--- index.js
+++ index.js
@@ -7,6 +7,66 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+app.command('/ticket', async ({ ack, body, client, logger }) => {
+  // Acknowledge the command request
+  await ack();
+
+  try {
+    // Call views.open with the built-in client
+    const result = await client.views.open({
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: body.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title',
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_',
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!',
+              },
+              action_id: 'button_abc',
+            },
+          },
+          {
+            type: 'input',
+            block_id: 'input_c',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true,
+            },
+          },
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+      },
+    });
+    logger.info(result);
+  } catch (error) {
+    logger.error(error);
+  }
+});
+
 // Function to convert Unix Epoch time to Korean date and time
 const convertToKoreanDateTime = (timestamp) => {
   const unixEpochTime = parseFloat(timestamp);
@@ -127,7 +187,6 @@ const isSOLog = (reply) => {
 const isScenarioEnd = (reply) => {
   return !reply.text.includes('시나리오가 마무리 되었습니다.');
 };
-
 
 app.shortcut('setupShortcuts', async ({ shortcut, ack, client }) => {
   // Acknowledge the shortcut request
